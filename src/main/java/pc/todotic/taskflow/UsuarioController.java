d2114f95b9eb96f8926f014608a8c6e98593b19a
Pedroascen <pedroascen3@gmail.com>
2023-07-09 22:48:25 -0600

    Implementacion de las operaciones CRUD con clase Tarea y Usuario

--- src/main/java/pc/todotic/taskflow/UsuarioController.java
+++ src/main/java/pc/todotic/taskflow/UsuarioController.java
@@ -1,44 +1,45 @@
 package pc.todotic.taskflow;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
-    @Autowired
-    private UsuarioRepository usuarioRepository;
-    //metodo para obtener lista de usuarios
-    @RequestMapping(value = "/api/usuarios", method = RequestMethod.GET)
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioController(UsuarioRepository usuarioRepository){//Mejora de la inyeccion de dependencia, para un codigo testeable
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @GetMapping//metodo para obtener lista de usuarios
     List<Usuario> listar(){
         return usuarioRepository.findAll();
     }
 
-    //metodo para obtener usuario por id
-    @RequestMapping(value = "/api/usuarios/{id}", method = RequestMethod.GET)
+    @GetMapping("/{id}")//metodo para obtener usuario por id
     Usuario obtener(@PathVariable Integer id){
         return usuarioRepository.findById(id).orElse(null);
     }
 
-    //metodo para crear nuevo usuario
-    @RequestMapping(value = "/api/usuarios", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping//metodo para crear nuevo usuario
     Usuario crear(@RequestBody Usuario usuario){
         usuarioRepository.save(usuario);
         return usuario;
     }
 
-    //metodo para actualizar usuario
-    @RequestMapping(value = "/api/usuarios/{id}", method = RequestMethod.PUT)
+    @PutMapping("/{id}")
     Usuario actualizar(@PathVariable Integer id, @RequestBody Usuario form){
         Usuario usuario = usuarioRepository.findById(id).orElse(null);
         usuario.setNombre(form.getNombre());
-        Usuario u = usuarioRepository.save(usuario);
-        return u;
+        return usuarioRepository.save(usuario);
     }
-
-    //metodo para eliminar usuario
-    @RequestMapping(value = "/api/usuarios/{id}",method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")//metodo para eliminar usuario
     void eliminar(@PathVariable Integer id){
         usuarioRepository.deleteById(id);
     }
